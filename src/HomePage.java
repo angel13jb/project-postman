bbffffa3db33c92ab44772a76400c0e38df46885
kukii77 <desty9008@gmail.com>
2022-05-31 21:14:31 +0100

    this commit is done by kinjal

--- src/HomePage.java
+++ src/HomePage.java
@@ -2,6 +2,7 @@ public class HomePage {
     public static void main(String[] args) {
         System.out.println("this is created by Jb");
         System.out.println("this is done by kinjal");
+        System.out.println("kinjal trail 2");
     }
 
 }
