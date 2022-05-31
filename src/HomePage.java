2720e2650b2d623a79777e1d51d2464cfbf0f204
angel13jb@gmial.com <angel13jb@gmail.com>
2022-05-31 20:02:52 +0100

    this is done by JB

---
+++ src/HomePage.java
@@ -0,0 +1,6 @@
+public class HomePage {
+    public static void main(String[] args) {
+        System.out.println("this is created by Jb");
+    }
+
+}
---
+++ src/LOginPage.java
@@ -0,0 +1,5 @@
+public class LOginPage {
+    public static void main(String[] args) {
+        System.out.println("project 1");
+    }
+}
