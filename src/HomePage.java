183917b077fa6bc2906b6fab7c8a1d0c80ae9301
angel13jb@gmial.com <angel13jb@gmail.com>
2022-05-31 21:50:26 +0100

    done by JB

--- src/HomePage.java
+++ src/HomePage.java
@@ -1,6 +1,7 @@
 public class HomePage {
     public static void main(String[] args) {
-        System.out.println("our last day in github");
+        System.out.println("last bye bye");
+
     }
 
 }
