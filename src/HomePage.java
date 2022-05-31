db1698707a5e6375e236e50ee43c7ab95761a8df
kukii77 <desty9008@gmail.com>
2022-05-31 21:33:29 +0100

    this is final commit

--- src/HomePage.java
+++ src/HomePage.java
@@ -1,11 +1,6 @@
 public class HomePage {
     public static void main(String[] args) {
-        System.out.println("this is created by Jb");
-        System.out.println("this is done by kinjal");
-        System.out.println("this is my own branch - kinjal");
-        System.out.println("This is Created by Megha");
-        System.out.println("this is again created by megha");
-        System.out.println("this is secound time by megha");
+        System.out.println("our last day in github");
     }
 
 }
